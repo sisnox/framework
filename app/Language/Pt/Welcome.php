fb0ac501cda1807f61d2ddb66ff580a34b55e2cd
sisnox <sisnox@gmail.com>
2016-04-15 00:02:57 +0100

    Portuguese language

---
+++ app/Language/Pt/Welcome.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * English language
+ */
+return [
+	// Index method
+	'welcomeText' => 'Bem-vindo',
+	'welcomeMessage' => '
+		Olá, seja bem-vindo a partir do controlador welcome! <br/>
+		Este conteúdo pode ser modificado em <code>/app/Views/Welcome/Welcome.php</code>
+	',
+
+	// Subpage method
+	'subpageText' => 'Sub-página',
+	'subpageMessage' => '
+		Olá, seja bem-vindo a partir do controlador welcome e do método subpage! <br/>
+		Este conteúdo pode ser modificado em  <code>/app/Views/Welcome/SubPage.php</code>
+	',
+
+	// Buttons
+	'openSubPage' => 'Abrir sub-página',
+	'backHome' => 'Início',
+];
